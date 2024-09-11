e3d60339350178874577d3da2080a082df211439
Rohit Menon <rohitm@telnyx.com>
2024-09-11 18:57:27 +0200

    Add /download_external_logs API call

--- backendApiStack/app.py
+++ backendApiStack/app.py
@@ -5,6 +5,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+EXTERNAL_INTGERATION_KEY = os.getenv('EXTERNAL_INTGERATION_KEY','')
+debug = os.getenv('FLASK_DEBUG', 'False')
 host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
 port = int(os.getenv('FLASK_RUN_PORT', 5000))
 debug = os.getenv('FLASK_DEBUG', 'False')
@@ -27,6 +29,15 @@ def api_call():
 @app.route('/health_check')
 def health_check():
     return f"healthy"
+
+## Adding a simple return for testing the existnce of environment can be expanded to accomadate return call od download API
+@app.route('/download_external_logs')
+def download_external_logs():
+    if EXTERNAL_INTGERATION_KEY == "":
+        return f"Missing API token", 403
+    else:
+        return f"Do API call", 200
+
 
 if __name__ == '__main__':
     app.run(debug=debug, host=host, port=port)
